7c8da9c6f0af18cd39acedec032843aec440de87
Sumshi <sumayasomow@gmail.com>
2024-03-16 21:32:24 +0300

    ✨ Page overflow fixed
    page overflow fixed

--- frontend/components/Home/Conversations.jsx
+++ frontend/components/Home/Conversations.jsx
@@ -57,7 +57,7 @@ export default function Conversations() {
 				</div>
 				<div className="">
 					<h3 className="text-md mt-4">Recent</h3>
-					<div className="overflow-auto scrollbar-thumb-slate-700 scrollbar-track-slate-300 scrollbar-thin h-[580px]">
+					<div className="overflow-auto scrollbar-thumb-slate-700 scrollbar-track-slate-300 scrollbar-thin h-[400px]">
 						{conversations.map((item) => (
 							<div
 								key={item._id}
