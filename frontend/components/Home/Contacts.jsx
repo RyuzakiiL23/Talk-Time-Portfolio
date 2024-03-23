b73e8d52d632b3f7e16f4c7c22d648f88dbff3d7
samit1719 <samuelteshome1719@gmail.com>
2024-03-23 20:32:35 +0300

    added search and select functionality

--- frontend/components/Home/Contacts.jsx
+++ frontend/components/Home/Contacts.jsx
@@ -1,15 +1,27 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { CiSearch } from "react-icons/ci";
+import Searchinput from "./Search";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { HiDotsVertical } from "react-icons/hi";
 import { useSocketContext } from "../../context/SocketContext";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	setMsg,
+	removeMsg,
+} from "@/lib/Features/Conversations/conversationSlice";
+import { setUserData } from "@/lib/Features/UsersData/userDataSlice";
+import { setInterlocuteur } from "@/lib/Features/Interlocuteur/interlocuteurSlice";
+
+import useListenMessages from "@/hooks/useListenMessages";
 
 export default function Contacts() {
 	const [conversations, setConversations] = useState([]);
 	const token = localStorage.getItem("chat-user");
 	const { onlineUsers } = useSocketContext();
+	const [userId, setUserId] = useState(null);
+	const dispatch = useDispatch();
+	useListenMessages();
 
 	useEffect(() => {
 		const getConversations = async () => {
@@ -35,46 +47,81 @@ export default function Contacts() {
 		getConversations();
 	}, []);
 
+	useEffect(() => {
+		const getMessages = async () => {
+			if (userId === null) {
+				dispatch(removeMsg());
+				return;
+			}
+			try {
+				const res = await fetch(
+					`http://localhost:8080/api/messages/${userId}`,
+					{
+						method: "GET",
+						credentials: "include",
+						headers: {
+							Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+							"Content-Type": "application/json",
+						},
+					}
+				);
+				const data = await res.json();
+				if (data.error) {
+					throw new Error(data.error);
+				}
+				dispatch(setMsg(data));
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getMessages();
+	}, [userId]);
+
 	return (
-		<div className="flex flex-col relative w-full border">
-			<div className="mx-4 ">
-				<h2 className="text-xl font-semibold my-4">Contacts</h2>
-				<div className="flex bg-[#E6EBF5] items-center h-10 rounded  ">
-					<div className="p-4 text-xl text-gray-500 ">
-						<CiSearch />
-					</div>
-					<input
-						className="focus:outline-none bg-[#E6EBF5] text-gray-500 text-sm "
-						type="text"
-						placeholder="Search messages or users"
-					/>
-				</div>
-				<div className="overflow-auto scrollbar-thumb-slate-700 scrollbar-track-slate-300 scrollbar-thin h-[78vh]">
-					{conversations.map((item) => (
-						<div
-							key={item._id}
-							className="flex cursor-pointer relative p-2 w-full rounded h-20 items-center"
-						>
-							<div className="mr-2 relative">
-								<div className={onlineUsers.includes(item._id) ? ' h-3 w-3 border rounded-full bg-green-500 absolute z-50' : ''}></div>
-								<Avatar className="h-8 w-8">
-									<AvatarImage
-										src={item.profilePic}
-										alt={item.username.slice(0, 1)}
-									/>
-									<AvatarFallback>{item.username.slice(0, 1)}</AvatarFallback>
-								</Avatar>
-							</div>
-							<div>
-								<h4 className="text-sm">{item.fullName}</h4>
-							</div>
-							<div className="absolute text-gray-500 text-xs right-2">
-								<HiDotsVertical />
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+    <div className="flex flex-col relative w-full border">
+      <div className="mx-4 ">
+        <h2 className="text-xl font-semibold my-4">Contacts</h2>
+        <div className="flex bg-[#E6EBF5] items-center h-10 rounded  ">
+          <Searchinput />
+        </div>
+        <div className="overflow-auto scrollbar-thumb-slate-700 scrollbar-track-slate-300 scrollbar-thin h-[78vh]">
+          {conversations.map((item) => (
+            <div
+              key={item._id}
+              className={`flex cursor-pointer relative p-2 w-full rounded h-20 items-center ${
+                userId === item._id ? "bg-[#E6EBF5]" : ""
+              }`}
+              onClick={() => {
+                setUserId(item._id);
+                dispatch(setInterlocuteur(item));
+              }}
+            >
+              <div className="mr-2 relative">
+                <div
+                  className={
+                    onlineUsers.includes(item._id)
+                      ? " h-3 w-3 border rounded-full bg-green-500 absolute z-50"
+                      : ""
+                  }
+                ></div>
+                <Avatar className="h-8 w-8">
+                  <AvatarImage
+                    src={item.profilePic}
+                    alt={item.username.slice(0, 1)}
+                  />
+                  <AvatarFallback>{item.username.slice(0, 1)}</AvatarFallback>
+                </Avatar>
+              </div>
+              <div>
+                <h4 className="text-sm">{item.fullName}</h4>
+              </div>
+              <div className="absolute text-gray-500 text-xs right-2">
+                <HiDotsVertical />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
