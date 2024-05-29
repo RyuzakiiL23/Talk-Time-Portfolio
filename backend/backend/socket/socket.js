480d9a8703e45b72703fda034be2c04f69012180
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-05-29 05:23:21 +0100

    vercell.config again

--- backend/backend/socket/socket.js
+++ backend/backend/socket/socket.js
@@ -9,10 +9,12 @@ const server = http.createServer(app);
 
 // Create a new instance of Socket.IO server and attach it to the HTTP server
 const io = new Server(server, {
-    cors: {
-        origin: ["https://talk-time-mu.vercel.app"],
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "https://talk-time-mu.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+    optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+  },
 });
 
 // Store the socket ID of each connected user
@@ -20,34 +22,34 @@ const userSocket = {};
 
 // Function to get the socket ID of a receiver based on their ID
 export const getReceiverSocketId = (receiverId) => {
-    return userSocket[receiverId];
+  return userSocket[receiverId];
 };
 
 // Event handler for new connections
 io.on("connection", (socket) => {
-    console.log("New connection", socket.id);
+  console.log("New connection", socket.id);
 
-    // Get the user ID from the handshake query
-    const userId = socket.handshake.query.userId;
+  // Get the user ID from the handshake query
+  const userId = socket.handshake.query.userId;
 
-    // Store the socket ID of the user
-    if (userId) {
-        userSocket[userId] = socket.id;
-    }
+  // Store the socket ID of the user
+  if (userId) {
+    userSocket[userId] = socket.id;
+  }
 
-    // Emit the list of online users to all connected clients
+  // Emit the list of online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocket));
+
+  // Event handler for disconnections
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+
+    // Remove the user's socket ID from the storage
+    delete userSocket[userId];
+
+    // Emit the updated list of online users to all connected clients
     io.emit("getOnlineUsers", Object.keys(userSocket));
-
-    // Event handler for disconnections
-    socket.on("disconnect", () => {
-        console.log("User disconnected", socket.id);
-
-        // Remove the user's socket ID from the storage
-        delete userSocket[userId];
-
-        // Emit the updated list of online users to all connected clients
-        io.emit("getOnlineUsers", Object.keys(userSocket));
-    });
+  });
 });
 
 export { app, server, io };
